28b5cd42826b136593e46fd6fad773c6f38eea73
Zeynab Movasaqpoor <30770159+zeynabmvs@users.noreply.github.com>
2024-07-22 11:31:54 -0700

    calculate total count

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,18 +1,25 @@
-import { useAppContext } from "@/app/context"
+import { useAppContext } from "@/app/context";
+import { useMemo } from "react";
 
-type CartPropTypes = {
-}
+type CartPropTypes = {};
 
 const Cart = (props: CartPropTypes) => {
-  const {cart} = useAppContext()
+  const { cart } = useAppContext();
+
+  const getTotalCount = ()=> {
+    return cart.reduce((total, item) => total + item.count, 0);
+  };
+
   return (
-    <div>Cart
-
-    {cart.map((item, index) => (<p key={index}>{item.product.name} - {item.count} </p>))}
-
+    <div className="bg-white rounded-[10px] py-8 px-6">
+      Your Cart ( {getTotalCount()} )
+      {cart.map((item, index) => (
+        <p key={index}>
+          {item.product.name} - {item.count}{" "}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-
-export default Cart
+export default Cart;
