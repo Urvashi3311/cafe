853242633596f383ebc3f617023110a497f9fe2f
zeynab.mvs <zeynab.mvs@gmail.com>
2024-08-28 11:06:40 -0700

    refactor framer motion code

--- app/components/CartItem.tsx
+++ app/components/CartItem.tsx
@@ -4,48 +4,38 @@ import { CartItemType } from "@/app/lib/definitions";
 import Button from "@/app/components/Button";
 import { useAppContext } from "@/app/context";
 import { formatDollar } from "@/app/utils";
-import { motion, Variants } from "framer-motion";
+import { motion } from "framer-motion";
+import { memo } from "react";
 
 type CartItemProps = {
   item: CartItemType;
 };
 
-const CartItem = (props: CartItemProps) => {
-  const item = props.item;
+const CartItem: React.FC<CartItemProps> = ({ item }) => {
   const { removeFromCart } = useAppContext();
+
   const cartItemTotal = item.quantity * item.product.price;
 
-  const containerVariants: Variants = {
+  const animations = {
     initial: { opacity: 0, y: 20 },
     animate: { opacity: 1, y: 0 },
     exit: { opacity: 0, x: 100, transition: { duration: 0.2 } },
-  };
-
-  const quantityVariants: Variants = {
-    initial: { opacity: 0 },
-    animate: { opacity: 1 },
-    exit: { opacity: 0 },
+    layout: true,
+    layoutId: `product_${item.product.id}`,
   };
 
   return (
     <motion.div
-      variants={containerVariants}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      layout
-      id="animated"
-      className="flex justify-between items-center border-b border-rose-100 py-3"
+      {...animations}
+      className="flex justify-between items-center border-b border-rose-100 py-3 bg-white"
     >
       <div className="flex flex-col gap-2">
         <h3 className="text-sm font-bold">{item.product.name}</h3>
         <div className="text-sm">
           <motion.span
             key={item.quantity}
-            variants={quantityVariants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             layout
             className="mr-4 text-red font-semibold"
           >
@@ -68,4 +58,4 @@ const CartItem = (props: CartItemProps) => {
   );
 };
 
-export default CartItem;
+export default memo(CartItem);
