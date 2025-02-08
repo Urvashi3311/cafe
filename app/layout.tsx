fe68301f6c0123a537c4701cff99d5fd857aeaea
Urvashi3311 <146938122+Urvashi3311@users.noreply.github.com>
2025-02-08 20:18:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Red_Hat_Text } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const redHatText = Red_Hat_Text({
   weight: ["400", "600", "700"],
@@ -18,12 +19,33 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
+   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/*  Google Analytics Tracking Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-F7YBFXQSBD`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F7YBFXQSBD', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${redHatText.className} text-base text-rose-900 bg-rose-50 relative`}
       >
